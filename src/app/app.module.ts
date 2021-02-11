6d80fc7b4e0f686ae8d99870b7be078132a853d5
polilla2802 <polilla2802@gmail.com>
2021-02-11 14:14:27 -0500

    header fix before icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
