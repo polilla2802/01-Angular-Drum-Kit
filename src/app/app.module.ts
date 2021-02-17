9f3ce6a74803c46c4500e2c5768c95d31c34e653
polilla2802 <polilla2802@gmail.com>
2021-02-17 10:18:03 -0500

    added key component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MatSliderModule } from '@angular/material/slider';
+import { KeyComponent } from './key/key.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/key/key.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key',
+  templateUrl: './key.component.html',
+  styleUrls: ['./key.component.css']
+})
+export class KeyComponent {
+
+  clicked = true;
+
+}
